9709cae98218093d126214e1ebcec59df070d454
Kayla Reid <iamkaylareid@gmail.com>
2018-08-10 11:34:15 -0500

    added some styles

--- src/js/entryComponet.js
+++ src/js/entryComponet.js
@@ -1,5 +1,5 @@
 const entryCards = (entryObject) =>{
-    return `<div id="entry-id--${entryObject.id}">
+    return `<div id="entry-id--${entryObject.id}" class="entry-card">
             <h2 id="title--${entryObject.id}">${entryObject.title}</h2>
             <h5 id="date--${entryObject.id}">${entryObject.date}</h5>
             <p id="content--${entryObject.id}">${entryObject.content}</p>
--- src/js/journalForm.js
+++ src/js/journalForm.js
@@ -9,10 +9,12 @@ const formManager = Object.create(null, {
     },
     makeForm: {
         value: () => {
-            return `<h2>Make a New Entry</h2>
+            return `<div class="form-div">
+            <h2 class="form-header">Make a New Entry</h2>
             <input required type="text" placeholder="Title of Entry" id="entryTitle">
             <textarea required placeholder="What's on your mind?" id="entryContent"></textarea>
-            <button id="add-entry-btn">Add Your Thoughts</button>`;
+            <button id="add-entry-btn">Add Your Thoughts</button>
+            </div>`;
         }
     }
 });
